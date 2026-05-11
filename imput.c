0bf79c10faa2d49f7f53ba73b702c91654b49b76
Rowan Oglesby <rowan.olgesby@student.nmt.edu>
2026-05-10 23:13:23 -0600

    This is the final commit I will do tonight. I will keep coding because this is not complete, and I really want to finish this project... Hope to have it done tomorrow. I am updating with my new journal, and the game code, the imput code, and the configuration code.

---
+++ imput.c
@@ -0,0 +1,82 @@
+/** 
+* @brief deals with all of the imputs from the player so they can play the game.
+*/
+
+#include "input.h"
+#include <stdlib.h>
+#define BOARD_ORIGIN_ROW 2
+#define CELL_WIDTH       2
+
+/**
+ * @brief Imputs for arrow keys and wasd keys
+ */
+static Action key_to_action(const struct ncinput *ni) {
+    switch (ni->id) {
+        case 'w': case NCKEY_UP:    return ACTION_UP;
+        case 's': case NCKEY_DOWN:  return ACTION_DOWN;
+        case 'a': case NCKEY_LEFT:  return ACTION_LEFT;
+        case 'd': case NCKEY_RIGHT: return ACTION_RIGHT;
+        case 'f':                   return ACTION_FLAG;
+        case 'c': case NCKEY_ENTER:
+        case '\n': case '\r':       return ACTION_CLEAR;
+        case 'q': case NCKEY_ESC:   return ACTION_QUIT;
+        default:                    return ACTION_NONE;
+    }
+}
+
+/** 
+ * @brief Just takes in the mouse imput and correlates it to board square.
+ * If outside of board does nothing.
+ */
+static bool mouse_to_board(int term_row, int term_col,
+                            int board_rows, int board_cols,
+                            int *out_row, int *out_col) {
+    int row = term_row - BOARD_ORIGIN_ROW;
+    int col = term_col / CELL_WIDTH;
+
+    if (row < 0 || row >= board_rows) return false;
+    if (col < 0 || col >= board_cols) return false;
+
+    *out_row = row;
+    *out_col = col;
+    return true;
+}
+
+/**
+*Waits for imput from player, either mouse or keys.
+ */
+InputEvent input_get(struct notcurses *nc, int board_rows, int board_cols) {
+    InputEvent ev = { .action = ACTION_NONE, .row = -1, .col = -1 };
+
+    struct ncinput ni;
+    notcurses_get_blocking(nc, &ni);
+
+    if (ni.evtype == NCTYPE_PRESS &&
+        (ni.id == NCKEY_BUTTON1 || ni.id == NCKEY_BUTTON3)) {
+
+        int br, bc;
+        bool on_board = mouse_to_board((int)ni.y, (int)ni.x,
+                                       board_rows, board_cols,
+                                       &br, &bc);
+        if (on_board) {
+            ev.row    = br;
+            ev.col    = bc;
+            ev.action = (ni.id == NCKEY_BUTTON1) ? ACTION_CLEAR : ACTION_FLAG;
+        }
+        return ev;
+    }
+
+    ev.action = key_to_action(&ni);
+    return ev;
+}
+
+/**
+*Checks to make sure the mouse works. If it doesnt then the arrow keys work.
+ */
+bool input_enable_mouse(struct notcurses *nc) {
+    return notcurses_mouse_enable(nc, NCMICE_BUTTON_EVENT) == 0;
+}
+
+void input_disable_mouse(struct notcurses *nc) {
+    notcurses_mouse_disable(nc);
+}
